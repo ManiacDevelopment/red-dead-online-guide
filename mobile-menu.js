4f06873dc2c7bc8397f689cad3ebbc7d899a0d91
ManiacDevelopment <markusmanihammer@gmail.com>
2026-05-11 19:46:41 +0200

    Create mobile-menu.js

---
+++ mobile-menu.js
@@ -0,0 +1,13 @@
+function toggleMobileMenu(){
+
+const menu =
+document.getElementById('mobileMenu');
+
+if(menu.style.display === 'block'){
+  menu.style.display = 'none';
+}
+else{
+  menu.style.display = 'block';
+}
+
+}
